eb1501ce11ae7b0791e5a78530d2d1aa1d67555a
asmahan <loveallah734@yahoo.com>
2018-08-10 17:45:41 +0200

    create summary pip and use it in contact item last message summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+// import components
 import { AppComponent } from './app.component';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { ChatBoxComponent } from './chat-box/chat-box.component';
 import { ContactItemComponent } from './contact-item/contact-item.component';
+
+// import pipes
+import {SummaryPipe} from './summary.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsListComponent,
     ChatBoxComponent,
-    ContactItemComponent
+    ContactItemComponent,
+    SummaryPipe
   ],
   imports: [
     BrowserModule,
