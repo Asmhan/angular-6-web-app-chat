9fff4d3e923ee14b5a3e804d5cbf08ca850e0c97
asmahan <loveallah734@yahoo.com>
2018-08-11 22:30:55 +0200

    fix animation and some styles

--- src/app/customstart.pipes.ts
+++ src/app/customstart.pipes.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 
-@Pipe({name: 'startsWith'})
+@Pipe({name: 'startsWith', pure: false})
 export class StartsWithPipe implements PipeTransform {
   transform(value: any[], term: string): any[] {
     return value.filter((x:any) => x.name.toLowerCase().startsWith(term.toLowerCase()));
